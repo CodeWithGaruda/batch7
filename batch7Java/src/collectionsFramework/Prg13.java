8db1e42449ebfda2e3ad6d0d247fc6ebaa9dad9b
CWG <CWG@gmail.com>
2022-04-09 16:29:03 +0530

    warnings

--- batch7Java/src/collectionsFramework/Prg13.java
+++ batch7Java/src/collectionsFramework/Prg13.java
@@ -1,7 +1,6 @@
 package collectionsFramework;
 
 import java.util.HashMap;
-import java.util.TreeMap;
 
 public class Prg13 {
     public static void main(String[] args) {
--- batch7Java/src/collectionsFramework/Prg9.java
+++ batch7Java/src/collectionsFramework/Prg9.java
@@ -4,10 +4,11 @@ public class Prg9 {
     public static void main(String[] args) {
         int a = 5;
         Integer num = a;// auto-boxing
-
         Integer i = 55;
         int ii = i.intValue();// unboxing
         System.out.println(ii);
         Integer integer = Integer.parseInt("5");
+
+        System.out.println(num + integer);
     }
 }
